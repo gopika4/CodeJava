b1f4f93303ab2dc0b703e18fb9eca53dddd1d0ab
gopika4 <41288338+gopika4@users.noreply.github.com>
2018-07-19 11:21:16 +0530

    Add files via upload

---
+++ Animals.java
@@ -0,0 +1,56 @@
+
+public class Animals {
+
+    	public void speak()
+    	{
+    	    System.out.println("Animals speaks");
+    	}
+}   	
+ class Cat extends Animals
+  {
+      public void speak()
+  	{
+  	    System.out.println("Meow Meow");
+  	}
+  }
+  class Dog extends Animals
+  {
+      public void speak()
+  	{
+  	    System.out.println("Bow Bow");
+  	}
+  }
+  
+  class Horse extends Animals{
+      public void speak()
+  	{
+  	    System.out.println("Heigh");
+  	}
+  }
+  
+class TestClass
+{
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+		String anam="Dog";
+		
+		animalnam(anam);
+		
+    }
+    public static void animalnam(String anam)
+    {
+	Animals a1 = null;
+	if(anam.equals("Cat"))
+	    a1=new Cat();
+	else if(anam.equals("Dog"))
+		a1=new Dog();
+	else  if(anam.equals("Horse"))
+		a1=new Horse();
+	anispeak(a1);
+	
+    }
+    public static void anispeak(Animals a)
+    {
+	a.speak();
+    }
+}
