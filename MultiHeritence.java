ce12d454ba344faaea35c36fbaf0f4d17be99281
gopika4 <41288338+gopika4@users.noreply.github.com>
2018-07-19 12:18:23 +0530

    Add files via upload

---
+++ MultiHeritence.java
@@ -0,0 +1,26 @@
+class A
+{
+    int a=50;
+}
+class B extends A
+{
+    int a=30;
+    int b=2;
+}
+class C extends B
+{
+    int c=20;
+}
+public class MultiHeritence {
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	C c1=new C();
+	System.out.println(c1.a);
+	System.out.println(((A)c1).a);
+	A a1=new C();
+	C c=(C)a1;
+	System.out.println(a1.a);
+	
+    }
+}
---
+++ casting.java
@@ -0,0 +1,46 @@
+class Emp
+{
+    
+}
+class Hr extends Emp
+{
+  
+}
+class Tech extends Emp
+{
+
+}
+class Recu extends Hr
+{
+    
+    
+}
+class Finan extends Hr
+{
+    
+}
+class Javapm extends Tech
+{
+    
+}
+class MainFrame extends Tech
+{
+    
+}
+public class casting {
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	Tech t =new Javapm();
+	Javapm jp=(Javapm)t;
+	
+	if (t instanceof Javapm) {
+	   System.out.println("Yes");
+	    
+	}
+	
+	
+	
+    }
+
+}
